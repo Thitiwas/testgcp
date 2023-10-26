dcb5fb6e485298893ace640fcc500f891bd4b7d5
zthitiwas.p <champ@MacBook-Pro-khxng-thitiwas.local>
2023-10-26 14:13:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var cors = require('cors')
+const app = express();
+
+app.use(cors('*'))
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.status(200).send('OK')
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
